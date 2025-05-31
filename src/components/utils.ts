f4510bdff86e75d1bf08cb99992744206865480a
Muhammad Haider Ali Khan <mmali277@gmail.com>
2025-05-31 17:39:27 +0300

    restructures examples and some functions

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -27,66 +27,6 @@ const createImage = (objectType:string): HTMLImageElement => {
 
 /**
  * Create a curved line
- * @param {CanvasRenderingContext2D} ctx canvas object
- * @param {{ x: number; y: number }} from starting x-axis and y-axis position
- * @param {{ x: number; y: number }} to ending x-axis and y-axis position
- * @param {number} curvature curvature of the line
- * @param {string} color background color of the line
- * @param {number} width width of the line
- * @param {ILabelOptions | undefined} labelOptions label to be displayed on the line
- * @returns {{ x: number; y: number }} from
- */
-export function drawCurvedLine(
-  ctx: CanvasRenderingContext2D,
-  from: { x: number; y: number },
-  to: { x: number; y: number },
-  curvature: number,
-  color: string,
-  width: number,
-  label?: string,
-  labelOptions?: ILabelOptions,
-) {
-  // Midpoint
-  const mx = (from.x + to.x) / 2;
-  const my = (from.y + to.y) / 2;
-
-  // Vector from start to end
-  const dx = to.x - from.x;
-  const dy = to.y - from.y;
-
-  // Rotate 90 degrees and apply curvature
-  const cx = mx + curvature * dy;
-  const cy = my - curvature * dx;
-  // // Perpendicular vector (normalized)
-  // const nx = -dy;
-  // const ny = dx;
-  // const len = Math.sqrt(nx * nx + ny * ny);
-  // const ux = nx / len;
-  // const uy = ny / len;
-
-  // // Control point (curved away from line)
-  // const cx = mx + ux * curvature * len;
-  // const cy = my + uy * curvature * len;
-  ctx.beginPath();
-  ctx.moveTo(from.x, from.y);
-  ctx.quadraticCurveTo(cx, cy, to.x, to.y);
-  // console.log('Libnk Color', color);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = width;
-  ctx.stroke();
-  if(labelOptions && label){
-    // Compute midpoint of curve using quadratic Bézier formula at t=0.5
-    const t = 0.5;
-    const x = (1 - t) ** 2 * from.x + 2 * (1 - t) * t * cx + t ** 2 * to.x;
-    const y = (1 - t) ** 2 * from.y + 2 * (1 - t) * t * cy + t ** 2 * to.y;
-    createLabel(label, ctx, labelOptions, x, y);
-  }
-
- 
-}
-
-/**
- * Create a curved line
  * @param {string} label label to be displayed
  * @param {CanvasRenderingContext2D} ctx canvas object
  * @param {IRelationLabelOptions | undefined} labelOptions label to be displayed on the line
